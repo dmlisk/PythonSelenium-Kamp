c6ad46db13d6acf87229f90609c5d1e6186ab80b
dmlisk <damlaissk@gmail.com>
2023-03-11 15:39:47 +0300

    hw1 oluşturuldu.

---
+++ Homework/hw1.py
@@ -0,0 +1,31 @@
+
+"""
+Soru 1:
+
+Python'da Veri Tipleri
+
+1. int: Tam sayıların içerisinde tutulduğu veri tipi'dir. Örneğin: 2, 18, 1923
+2. float: Ondalıklı sayıların içerisinde tutulduğu veri tipi'dir. Örneğin: 11.5, 48.7 
+3. str: Metinsel ifadelerin yani yazıların veya karakterlerin içerisinde tutulduğu veri tipidir. 
+Değerler çift tırnak veya tek tırnak içerisine yazılarak kullanılır.
+4. bool: Mantıksal bir veri tipidir. İçerisine true ya da false değeri alabilmektedir. 
+5. complex: Complex veriler (5j + 1) gibi ifadelerdir. Bu verilerin gerçek ve sanal kısımları vardır.
+6. list: Sıralı veri tipleridir. İçinde farklı veri tipleride bulundurabilir. Değiştirilebilirler.
+7. tuple: Liste yapısına benzerdir. list den farkı değiştirilemez olmalarıdır.
+8. range: Belirli bir aralıktaki sayıları saklar.
+
+Soru 2: Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.
+
+String --> Kurslarım, Tüm Kurslar, Kariyer, Sık Sorulan Sorular, Alınan Kursların isimleri
+Int --> Tamamlanan kursların yüzdesi
+
+"""
+
+#Soru 3: Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz.
+
+dersTamamlandi = True
+
+if dersTamamlandi:
+    print("Ders Tamamlandı, bir sonraki derse geçebilirsin.")
+else:
+    print("Ders tamamlanmadı.")
